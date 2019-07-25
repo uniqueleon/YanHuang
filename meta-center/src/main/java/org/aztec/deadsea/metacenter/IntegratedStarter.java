d727f38de13ba273f2414a8177b7db17aee16ae5
uniqueleon <dawnfarrel@126.com>
2019-07-25 13:56:12 +0800

    update gitignore

--- meta-center/src/main/java/org/aztec/deadsea/metacenter/IntegratedStarter.java
+++ meta-center/src/main/java/org/aztec/deadsea/metacenter/IntegratedStarter.java
@@ -2,6 +2,6 @@ package org.aztec.deadsea.metacenter;
 
 public class IntegratedStarter {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World!a");
 	}
 }
