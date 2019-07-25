dd299e0371bccf973064b0cdc7d505a594bc39f3
uniqueleon <dawnfarrel@126.com>
2019-07-25 13:54:39 +0800

    忽略无用文件

--- common/src/main/java/org/aztec/deadsea/common/xa/ProposalFactory.java
+++ common/src/main/java/org/aztec/deadsea/common/xa/ProposalFactory.java
@@ -5,4 +5,5 @@ import java.util.Map;
 public interface ProposalFactory {
 
 	public XAProposal createProposal(String txID,String type,int quorum,Map<String,Object> attachments)  throws XAException;
+	public XAProposal rebuild(XAContext context) throws XAException;
 }
